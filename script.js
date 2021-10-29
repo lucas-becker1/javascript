db709f3c9d1c38322d69498260398e31c09223de
lucas.becker <lucas.becker.14.lb@gmail.com>
2021-10-28 21:44:55 -0300

    desafio 4

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let numero1 = parseFloat (prompt("Ingrese un numero"))
-
-for(let i = 5; i<= 15; i++) {
-    console.log(`Numero ${numero1} sumado mas ${i} es igual a ${numero1 + i}`)
+function suma(numero1, numero2) {
+    return (numero1 / numero2)
 }
+
+let numero1 = parseFloat(prompt("Ingrese el precio del producto"))
+let numero2 = parseFloat(prompt("Ingrese la cantidad de cuotas que desea: 3, 6, 9 o 12"))
+
+alert("El precio final de cada cuota es de $" + suma(numero1, numero2))
+
+/*const IVA = 1.21;
+
+function sumarIVA (producto) {
+    return producto * IVA
+}
+
+let producto = parseFloat(prompt("Ingrese el precio del producto"))
+
+alert("El precio final es de $" + sumarIVA(producto))*/

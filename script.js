f0117111f778aca4254f3edbecf8ebf93349dd84
lucas.becker <lucas.becker.14.lb@gmail.com>
2021-11-04 21:58:30 -0300

    preentrega

--- script.js
+++ script.js
@@ -1,72 +1,115 @@
-function Producto(nombreProducto, talle, tela, color) {
+alert("¡Bienvenidos a Camisetea2")
+// Creo dos const para que el cliente pueda ingresar sus datos
+let cantidadJ = parseInt(prompt("Ingrese cuantos clientes desea registrar: "))
+for(let i=0; i < cantidadJ; i++){
+    let nombre = prompt("Ingrese nombre: ")
+    let apellido = prompt("Ingrese apellido: ")
+    console.log("Cliente: ")
+    console.log("-" + nombre + " " + apellido)
+    }
+
+    alert("¡Registro exitoso!")
+
+
+alert("Tenemos 5 combos para vos")
+
+// Creo una funcion donde registro cada uno de mis productos
+function Producto(nombreProducto, talle, color) {
     this.nombreProducto = nombreProducto;
     this.talle = talle;
-    this.tela = tela;
     this.color = color;
-    this.mostrar = () => console.log("Productos Registrados");
+    this.mostrar = () => console.log("Stock disponible");
     this.retorna = () => `${this.nombreProducto} ${this.talle}`
 }
 
+// Creo 5 const donde utilizo la funcion producto para poder registrar mis 5 productos
+const producto1 = new Producto("camiseta", "L", "azul")
+const producto2 = new Producto("short", "M", "azul")
+const producto3 = new Producto("medias", "L", "blanco")
+const producto4 = new Producto("pantalon", "L", "celeste")
+const producto5 = new Producto("campera", "XL", "celeste")
 
-const producto1 = new Producto("remera", "M", "jersey", "blanco")
-const producto2 = new Producto("chomba", "L", "pique", "rojo")
-const producto3 = new Producto("campera", "XL", "jersey", "azul")
-const producto4 = new Producto("buzo", "M", "friza", "negro")
-const producto5 = new Producto("pantalon", "L", "friza", "azul")
-
-
-console.log(producto1)
-console.log(producto2)
-console.log(producto3)
-console.log(producto4)
-console.log(producto5)
+// Muestro que los productos fueron registrados correctamente
 producto1.mostrar()
 producto2.mostrar()
 producto3.mostrar()
 producto4.mostrar()
 producto5.mostrar()
 
+// Creo arrays para ofrecer 5 combos de mis productos
+const combo1 = ["Camiseta + " + "Short "];
+const combo2 = ["Short + " + "Medias "];
+const combo3 = ["Campera + " + "Pantalon "];
+const combo4 = ["Camiseta + " + "Short + " + "Medias "];
+const combo5 = ["Campera + " + "Pantalon + " + "Medias "];
 
+// Utilizo la propiedad lenght para saber cuantos productos tiene cada combo
+console.log( "El combo1 contiene: " + combo1.lenght + " productos y son: " + combo1);
+console.log( "El combo2 contiene: " + combo2.lenght + " productos y son: " + combo2);
+console.log( "El combo3 contiene: " + combo3.lenght + " productos y son: " + combo3);
+console.log( "El combo4 contiene: " + combo4.lenght + " productos y son: " + combo4);
+console.log( "El combo5 contiene: " + combo5.lenght + " productos y son: " + combo5);
 
-function Egresado(nombre, apellido, talle) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.talle = talle;
-    this.mostrar = () => console.log("¡Hola!");
-    this.retorna = () => `${this.nombre} ${this.apellido}`
+// Utilizo el push para agregar un elemento a mi combo
+combo1.push(" Campera");
+console.log("El combo1 queda: "+ combo1);
+
+//Funcion para saber lo que contiene cada combo, el usuario ingresa que combo desea y muestro con un alert lo que este contiene
+let opera = prompt("Ingrese: ( combo1, combo2, combo3, combo 4, combo5 ) ")
+
+function combo_ingresado(combo1,combo2,combo3,combo4,combo5,opera) {
+
+    switch(opera) {
+        case "combo1":
+            alert("El Combo1 contiene: "+ (combo1))
+            break
+        case "combo2":
+            alert("El Combo2 contiene: "+ (combo2))
+            break
+        case "combo3":
+            alert("El Combo3 contiene: "+ (combo3))
+            break
+        case "combo4":
+            alert("El Combo4 contiene: "+ (combo4))
+            break
+        case "combo5":
+            alert("El Combo5 contiene: "+ (combo5))
+            break
+    }
 }
 
+// Llamo a la funcion combo_ingresado para que se ejecute
+combo_ingresado(combo1,combo2,combo3,combo4,combo5,opera)
 
-const egresado1 = new Egresado (prompt("Ingrese nombre: "), prompt("Ingrese apellido: "), prompt("Ingrese talle: "))
-const egresado2 = new Egresado (prompt("Ingrese nombre: "), prompt("Ingrese apellido: "), prompt("Ingrese talle: "))
-
-
-console.log(egresado1)
-egresado1.mostrar()
-console.log(egresado2)
-egresado2.mostrar()
-
-
-class Vaso {
-        constructor(tipoVaso, colorVaso) {
-            this.tipoVaso = tipoVaso;
-            this.colorVaso = colorVaso;
-        }
-
-        tipo(modelo) {
-            console.log(`El vaso ${this.tipoVaso} que le corresponde es de color: ${this.colorVaso} `)
-        }
+// Creo una clase bandera para crear banderas
+class bandera {
+    constructor(numeroBandera, tamaño, color) {
+        this.numeroBandera = numeroBandera;
+        this.tamaño = tamaño;
+        this.color = color;
     }
 
+    mostrarCaracteristicas () {
+        console.log(`Bandera n°:${this.numeroBandera} cm:${this.tamaño} color:${this.color}`)
+    }
+}
 
-const vaso1 = new Vaso("de café", "Negro")
-const vaso2 = new Vaso("de entrenamiento", "Blanco")
+// Creo 2 const para poner la in
+const bandera1 = new bandera (20, 200, "Azul")
+const bandera2 = new bandera (14, 250, "Roja")
+
+// Muestro por consola que banderas les corresponden de regalo
+console.log("Las banderas de regalo que le corresponde son: ")
+bandera1.mostrarCaracteristicas()
+bandera2.mostrarCaracteristicas()
+
+alert("Su compra y regalos fueron procesados, gracias por elegirnos!")
 
 
-console.log(vaso1)
-vaso1.tipo("de café")
-console.log(vaso2)
-vaso2.tipo("de entrenamiento")
+
+
+
+
 
 
 

465dde33307fe5e32c938128d718b83c94032d13
lucas.becker <lucas.becker.14.lb@gmail.com>
2021-11-17 00:51:38 -0300

    noveno desafio

--- script.js
+++ script.js
@@ -1,72 +1,93 @@
-alert("¡Bienvenido/a a Camisetea2!")
-// Aqui se registra el cliente
-let nombre = prompt("Ingrese nombre: ")
-let apellido = prompt("Ingrese apellido: ")
-console.log("Cliente: " + nombre + " " + apellido)
-
-alert("¡Registro exitoso!")
-
-// Creo una funcion para los productos
-function Producto(nombre, talle, precio) {
-    this.nombre = nombre;
-    this.talle = talle;
-    this.precio = parseFloat(precio);
-    this.disponible = true;
-}
-
-// Creo un array para que el cliente elija sus productos
-var arrayProductos = [];
-do {
-    var comprobacion = prompt("Ingrese el nombre del producto que desea o FIN para terminar de agregar");
-    if (comprobacion === "fin" || comprobacion === "FIN" || comprobacion === "Fin"){
-        break;
-    }else{
-        nombreP = comprobacion;
-        var talleP = prompt("Ingrese el talle del producto");
-        var precioP = prompt("Ingrese el precio del producto");
-        arrayProductos.push(new Producto(nombreP, talleP, precioP));
-    }
-}
-while (comprobacion != "fin" || comprobacion != "FIN" || comprobacion != "Fin" )
-
-console.log(arrayProductos);
-
-
-for (var producto of arrayProductos) {
-    console.log(producto.nombre);
-    console.log(producto.talle);
-    console.log(producto.precio);
-}
-
-// Aqui se ordenan los productos segun su precio (en caso de ser mas de uno)
-var ordenadosPrecio = [];
-ordenadosPrecio = arrayProductos.map (elemento => elemento);
-ordenadosPrecio.sort(function(a, b) {
-    return a.precio - b.precio;
-});
-console.log("Ordenados por precios ascendentes:");
-console.log(ordenadosPrecio);
-
-// Creo una clase para crear banderas
-class Bandera {
-    constructor(numeroBandera, tamaño, color) {
-        this.numeroBandera = numeroBandera;
-        this.tamaño = tamaño;
-        this.color = color;
-    }
-
-    mostrarCaracteristicas () {
-        console.log(`Bandera N°:${this.numeroBandera} CM:${this.tamaño} Color:${this.color}`)
+class Alumno {
+    constructor(nombre, apellido, materia) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.materia = materia
     }
 }
 
-// Creo 2 const para poner la in
-const bandera1 = new Bandera (1, 200, "a eleccion")
-const bandera2 = new Bandera (2, 250, "a eleccion")
+let arrayAlumnos = [];
+let miFormulario = document.querySelector("#formulario");
+let inputNombre = document.querySelector("#iNombre");
 
-// Muestro por consola que banderas le corresponden de regalo al cliente
-console.log("Las banderas que le corresponde de regalo son: ")
-bandera1.mostrarCaracteristicas()
-bandera2.mostrarCaracteristicas()
+let nombreI = formulario.children[1].value;
+let apellidoI = formulario.children[1].value;
+let materiaI = formulario.children[1].value;
 
-alert("Su compra y regalos fueron procesados, gracias por elegirnos!")
+let contenedor = document.querySelector("#alumnoIngresado");
+let displayTodos = document.querySelector("#displayTodos");
+let parrafos = displayTodos.getElementsByTagName("p");
+let bandera = false;
+
+
+miFormulario.addEventListener("submit", agregarAlumnos);
+btnMostrar.addEventListener('click', MostrarTodosAlumnos);
+
+
+inputNombre.focus();
+
+
+function validarForm() {
+
+    nombreI = formulario.children[1].value;
+    apellidoI = formulario.children[1].value;
+    materiaI = formulario.children[1].value;
+    console.log(nombreI);
+    console.log(apellidoI);
+    console.log(materiaI);
+
+    if (nombreI == '' || apellidoI == '' || materiaI == '') {
+        alert('ERROR - Complete todos los campos para continuar');
+        inputNombre.focus();
+        bandera = false;
+    }   else {
+        bandera = true;
+    }
+}
+
+
+function agregarAlumnos (e) {
+    e.preventDefault();
+    validarForm();
+    if (bandera == true) {
+        let opcion = confirm("¿Seguro que desea agregar al alumno?");
+        if (opcion == true) {
+            let formulario = e.target
+            arrayAlumnos.push(new Alumno(nombreI, apellidoI, materiaI));
+        } else {
+            alert('No se agregará al alumno');
+        }
+    formulario.children[1].value = '';
+    formulario.children[1].value = '';
+    formulario.children[1].value = '';
+    contenedor.innerHTML = '';
+    AgregarAlDom();
+    inputNombre.focus();
+    } else {
+    inputNombre.focus();
+    }
+}
+
+
+function AgregarAlDom() {
+    contenedor.innerHTML = `<h3> Ultimo alumno ingresado:</h3>
+    <p><strong> Nombre: </strong> ${nombreI}</p>
+    <p><strong> Apellido: </strong> ${apellidoI} </p>
+    <p><strong> Materia: </strong> ${materiaI}</p>
+    </hr>`;
+}
+
+
+function MostrarTodosAlumnos (e) {
+    e.preventDefault();
+    let i = 0;
+    displayTodos.innerHTML = '<h3> Listado de todos los alumnos:</h3>';
+    for (const alumno of arrayAlumnos) {
+
+        displayTodos.innerHTML += `
+        <p><strong> Nombre: </strong> ${nombreI}</p>
+    <p><strong> Apellido: </strong> ${apellidoI} </p>
+    <p><strong> Materia: </strong> ${materiaI}</p>
+    </hr>`;
+    }
+}

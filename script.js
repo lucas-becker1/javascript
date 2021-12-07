2603640a7533a74caf430192ebd240e5ac26314c
lucas.becker <lucas.becker.14.lb@gmail.com>
2021-12-07 16:12:33 -0300

    pre entrega 3

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
+let darkMode;
+
+if(localStorage.getItem('dark-mode')) {
+    darkMode = localStorage.getItem('dark-mode')
+} else {
+    darkMode = "light"
+}
+
+localStorage.setItem('dark-mode', darkMode)
+
+$(() => {
+    if(localStorage.getItem('dark-mode') == "dark") {
+        $('body').addClass("dark")
+        $('#boton-dark-mode').hide()
+        $('#boton-light-mode').show()
+    } else {
+        $('#boton-light-mode').hide()
+    }
+
+    $('#boton-light-mode').click(() => {
+        $('#boton-light-mode').hide()
+        $('#boton-dark-mode').show()
+        $('body').removeClass("dark")
+
+        localStorage.setItem('dark-mode', "light")
+    })
+
+
+    $('#boton-dark-mode').click(() => {
+        $('#boton-light-mode').show()
+        $('#boton-dark-mode').hide()
+        $('body').addClass("dark")
+        localStorage.setItem('dark-mode', "dark")
+    })
+
+    $('#boton1').click(function() {
+        $('#box').animate({
+            height: "200px",
+            width: "200px",
+            backgroundColor: '#082AD9'
+        }, {
+            duration: 1000,
+            easing: "linear",
+            complete: () => {
+                console.log(this)
+            }
+        });
+    })
+
+    $('#p1').animate({
+        "font-size": "40px",
+        "padding": "30px",
+        "color": "#1C34B0",
+        "width": "140px"},
+        "fast",
+        function() {
+            console.log("Animacion terminada")
+        }
+    ).fadeOut(5000).delay(1000).fadeIn(5000)
+    
+    $('#p2').fadeOut('slow', () => {
+        $('#p2').fadeIn(1000)
+    })
+})
+
 class Camiseta {
     constructor(id, equipo, temporada, talle) {
         this.id = id;
